8c9ee8bf8f95f0636b1cc4ff36bd94a0a9b7376a
david-shakaya <devidoz666@gmail.com>
2020-12-24 20:40:27 +0200

    +

--- src/templates/modul12.js
+++ src/templates/modul12.js
@@ -1,3 +1,7 @@
+
+import articlesTempl from '../templates/articles.hbs'
+
+
 // Таким образом ищем чтото на  бекенде (ПУБЛИЧНОМ API)
 // Метод может быть и не json() - все зависит от того акие файлы с бекенда к нам приходят
 
@@ -16,17 +20,43 @@
 
 /* >>>>>>>>>>>>>>>>>
  */
+const refs ={
+    articlesContainer: document.querySelector('.articles-js'),
+    searchForm: document.querySelector('.search-form-js'),
+
+}
+
+
     //  Публичный API (Бекенд) - на котором требуется авторизация
     // Как работать с этим API можно найти в документации
 // fetch('http://newsapi.org/v2/top-headlines?country=ua&q=19&apiKey=9612ce51c4354d579a8c97e2de8f4083')
 //     .then(res => res.json())
 //     .then(data => console.log(data.articles))
 
-//>>>>>>>> ТО же самое но делаем переменные(типа красивый код)
-const url = 'http://newsapi.org/v2/top-headlines?country=ua&q='
-let findNews = '19'
-const key = '&apiKey=9612ce51c4354d579a8c97e2de8f4083'
-fetch(`${url}${findNews}${key}`)
-    .then(res => res.json())
-    .then(data => console.log(data.articles))
+// >>>>>>>> ТО же самое но делаем переменные(типа красивый код)
+// const url = 'http://newsapi.org/v2/top-headlines?country=ua&q='
+// let findNews = ''
+// const key = '&apiKey=9612ce51c4354d579a8c97e2de8f4083'
+// fetch(`${url}${findNews}${key}`)  
+//     .then(res => res.json())
+//     .then(data => {
+//         const markup = articlesTempl(data.articles)
+//         refs.articlesContainer.insertAdjacentHTML('beforeend',markup)
+//     })
+    
+    // НУЖНО КОД ПОЧИСТИТЬ КАК В ВИДЕО РЕПЕТЫ
+refs.searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputValue = e.target.query.value
 
+     refs.articlesContainer.innerHTML = ''
+    const url = 'http://newsapi.org/v2/top-headlines?country=ua&q='
+ let findNews = inputValue
+const key = '&apiKey=9612ce51c4354d579a8c97e2de8f4083'
+fetch(`${url}${findNews}${key}`)  
+    .then(res => res.json())
+    .then(data => {
+        const markup = articlesTempl(data.articles)
+        refs.articlesContainer.insertAdjacentHTML('beforeend',markup)
+    })
+})
